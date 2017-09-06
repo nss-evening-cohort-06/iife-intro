76fe45b2654e3175bc5719051cd6b33a3fc932b9
Zoe Ames <zoeames@gmail.com>
2017-09-05 19:20:00 -0500

    basic iife added

--- main.js
+++ main.js
@@ -1,1 +1,42 @@
-console.log("hello")
+// let Something = (function(oldSomething){
+// 	const animals = ["cat", "dog"];
+
+// 	oldSomething.niceFunction = function(){
+// 		console.log("im a nice function");
+// 	}
+
+// 	return oldSomething;
+
+// })(Something || {});
+
+
+var SolarSystem = (function(oldSolarSystem){
+	const planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
+	const spacecraft = ["Voyager", "Gemini", "Space Shuttle"];
+
+	oldSolarSystem.getPlanets = function(){
+		return planets;
+	};
+
+	oldSolarSystem.setPlanet = function(newPlanet){
+		planets.push(newPlanet);
+	};
+
+	oldSolarSystem.getSpacecraft = function(){
+		return spacecraft;
+	};
+
+	oldSolarSystem.setSpacecraft = function(newSpacecraft){
+		spacecraft.push(newSpacecraft);
+	};
+
+	return	oldSolarSystem;
+})(SolarSystem || {});
+
+
+let myPlanets = SolarSystem.getPlanets();
+console.log("myPlanets", myPlanets);
+
+
+
+
